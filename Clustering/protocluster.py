ce1fd78d677a47bac65ef03569d0f191ac4ee302
grossora <grossora.neutrino@gmail.com>
2016-09-17 11:48:55 -0500

    clean-ish cluster code-- ryanW

--- Clustering/protocluster.py
+++ Clustering/protocluster.py
@@ -50,104 +50,71 @@ def dbscan(inup, dist, minsample ):
     #db = DBSCAN(eps=dist, min_samples=minsample).fit(inup)
     labels = db.labels_
     return labels
+    
 
 ############################################
 ######### crawler Clustering ###############
 ############################################
 def crawler(inup, dist, mincluster ):
-    indexlist = [-1 for x in range(len(inup))] 
-    unusedlist = [x for x in range(len(inup))] 
+    indexlist = [-1 for x in range(len(inup))]
+    unusedlist = [x for x in range(len(inup))]
     clusterlabel = 0
     mindist = pow(dist,2) # This will save us from computing a square root on 
     for pt in range(len(inup)):
-	#see if this point is already used
-	if not pt in unusedlist:
-	    continue
-	# Make a temp list for potential merged points 
-	tmpmerge = []
-	tmpmerge.append(pt)
-	#Push back on a temp list... next version#### RG
-	mergedpts = False# This is here now.. we can remove this later
+        #see if this point is already used
+        if not pt in unusedlist:
+            continue
+        # Make a temp list for potential merged points 
+        tmpmerge = []
+        tmpmerge.append(pt)
+        #Push back on a temp list... next version#### RG
+        mergedpts = False# This is here now.. we can remove this later
 
-	# remove pt from unused list 
-	ptindex = unusedlist.index(pt)
-	unusedlist.pop(ptindex)
+        # remove pt from unused list 
+        ptindex = unusedlist.index(pt)
+        unusedlist.pop(ptindex)
 
-	# Now crawthough all the rest of the points
-	boo = True    ### Is this needed? 
-	checklast = [x for x in tmpmerge]
-	while boo: # This is going to take long... but it's like a clean up
-	    tmp_copy = len(tmpmerge)	
-	    tcl = []
-	    for testpt in unusedlist:
-	        # Check distances  
-	        # here we are going to check all the list of points in the tmp until we either end of find a close point
-	        for other in checklast: 
-	        #for other in tmpmerge: 
-	            distsqrd = pow(inup[other][0]-inup[testpt][0],2) + pow(inup[other][1]-inup[testpt][1],2) + pow(inup[other][2]-inup[testpt][2],2) 
-	            if distsqrd<mindist:
-	 	        #Merge them into the current cluster 
-		        #The index in index list becomes whatever the cluster counter is
-		        tmpmerge.append(testpt)
-		        tcl.append(testpt)
-			break
-		        #now get out and move to next point to consider
-		# Reset the checklast from tcl 
-	    checklast = [ x for x in tcl]
+        # Now crawthough all the rest of the points
+        boo = True    ### Is this needed? 
+        while boo: # This is going to take long... but it's like a clean up
+            tmp_copy = len(tmpmerge)
+            for testpt in unusedlist:
+                # Check distances  
+                # here we are going to check all the list of points in the tmp until we either end of find a close point
+                for other in tmpmerge:
+                    distsqrd = pow(inup[other][0]-inup[testpt][0],2) + pow(inup[other][1]-inup[testpt][1],2) + pow(inup[other][2]-inup[testpt][2],2)
+                    if distsqrd<mindist:
+                        #Merge them into the current cluster 
+                        #The index in index list becomes whatever the cluster counter is
+                        tmpmerge.append(testpt)
+                        #now get out and move to next point to consider
+                        break
+            if len(tmpmerge)>mincluster:# this is not the best way to do this but for now its ok 
+            # if we make it to pass the if that means we are going to make cluster
+            # so it does not matter if we clean up the unusedlist here 
+                for s in tmpmerge:
+                    if s ==0:# this is the pt and it is not in unused
+                        continue
+                    # Check if it is in the list
+                    if s in unusedlist:
+                        # clear it out of the unused list since it is not used
+                        iv = unusedlist.index(s)
+                        unusedlist.pop(iv)
 
+            # if we are at a steady state then get out of this
+            if tmp_copy==len(tmpmerge):
+                boo = False # is this needed? Wont break just take us out? 
+                break
 
-	    if len(tmpmerge)>mincluster:# this is not the best way to do this but for now its ok 
-	    # if we make it to pass the if that means we are going to make cluster
-	    # so it does not matter if we clean up the unusedlist here 
-	        for s in tmpmerge: 
-		    if s ==0:# this is the pt and it is not in unused
-			continue
-		    # Check if it is in the list
-		    if s in unusedlist:
-			# clear it out of the unused list since it is not used
-		        iv = unusedlist.index(s)
-		        unusedlist.pop(iv)
-		
-	    # if we are at a steady state then get out of this
-	    if tmp_copy==len(tmpmerge):
-		boo = False # is this needed? Wont break just take us out? 
-		break
-	
-
-	if len(tmpmerge)>=mincluster:
-	    #label the points 
-	    for s in tmpmerge:
-		indexlist[s] = clusterlabel	
-	    clusterlabel+=1
-	    #remove from unused list 
-
-	    # We already did this
-	# we need a catch to put back the tmpperge points into unused if we do not pass the min cluster
-##########
-#	if len(tmpmerge)<mincluster:
-#	    for b in tmpmerge:
-#		unusedlist.append(b)
-##########   
-	
+        if len(tmpmerge)>=mincluster:
+            #label the points 
+            for s in tmpmerge:
+                indexlist[s] = clusterlabel
+            clusterlabel+=1
+            #remove from unused list 
+        # we need a catch to put back the tmpperge points into unused if we do not pass the min cluster
+#########   
     return indexlist
-		
-	
-
-
-
-
-
-
-
-
-
-
-
-
- 
-
-
-
 
 
 
