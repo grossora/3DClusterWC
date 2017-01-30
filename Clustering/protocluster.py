3370386d83bbe27023e245f68c7810dfada9a123
grossora <grossora.neutrino@gmail.com>
2017-01-30 07:56:45 -0600

    updated .... clean next

--- Clustering/protocluster.py
+++ Clustering/protocluster.py
@@ -59,6 +59,54 @@ def dbscan(inup, dist, minsample ):
     #db = DBSCAN(eps=dist, min_samples=minsample).fit(inup)
     labels = db.labels_
     return labels
+
+def walker(inup, dist, mincluster):
+    idxlist = [-1 for x in range(len(inup))]
+    punused = [x for x in range(len(inup))]
+    tdist = dist*dist
+    idxcounter = 0
+
+    # Sort the unused list 
+    punused_z = [inup[i][2] for i in punused]
+    sort_idx = sorted(range(len(punused_z)), key=lambda k: punused_z[k])
+    unused = [punused[idx] for idx in sort_idx]
+
+    for i in unused:
+        # i is the first point
+        if idxlist[i] != -1:
+            continue
+        tmpclust = []# make none
+        tmpclust.append(i)
+        max_z = inup[i][2]
+    #print ' start of the i loop-------'
+        for j in unused:
+            if idxlist[j] !=-1:
+                continue
+            if idxlist[j] == -1:
+                if inup[j][2] - max_z >dist:
+                #           print 'we are breaking out early'
+                    break
+            for te in tmpclust:
+                sqdist_test = pow(inup[j][0]-inup[te][0],2)+pow(inup[j][1]-inup[te][1],2)+pow(inup[j][2]-inup[te][2],2)
+                if sqdist_test<tdist:
+                    # add j to tmpclust
+                    tmpclust.append(j)
+                    if max_z<inup[j][2]:
+                        max_z = inup[j][2]
+                    break
+        #print len(tmpclust)
+    # log the idxlist
+        if len(tmpclust)>mincluster:
+            for lab in tmpclust:
+                idxlist[lab] = idxcounter
+            idxcounter +=1
+    return idxlist
+
+
+def walker_2(inup, dist, mincluster):
+    idxlist = [-1 for x in range(len(inup))]
+    return idxlist
+
     
 
 ############################################
@@ -249,11 +297,327 @@ def hull_check(points):
         return 0
     # This really needs to be n+1 for checking dimensionality.... but we might get lucky ror now.
 
+ 
+def Track_Stitcher_RenameGood(dataset,datasetidx_holder,labels):
+    # Clean Stitch 
+
+    # min_dist ==> min dist for the vertex points of two different hulls to be considered
+    min_dist = 10  # RG Magic
+    # k_radius ==> radius of the sphere around the vertex point looking at clustered charge for that hull
+    k_radius = 10  # RG Hard Coded
+    max_delta = 5. # Magic.... settable
+    CHQ_vec = []
+    # ^^^^^^ Will be of the form [    [datasetidx_holder INDEX, the vertices of the hull, total_charge] ]
+
+    for a in range(len(datasetidx_holder)):
+        points_v = []
+        tot_q = 0.0
+	
+        for i in datasetidx_holder[a]:
+	    if labels[i]==-1:
+		print 'LOOOOOOOOOOOOOOOOOOOK -1  '
+		break
+            pt = [ dataset[i][0],dataset[i][1],dataset[i][2]]
+            points_v.append(pt)
+            tot_q+= dataset[i][3]
+
+	#Try to make a hull 
+	try: 
+            hull = ConvexHull(points_v)
+	except:
+	    continue 
+
+        # Now we have the hull
+        ds_hull_idx = [datasetidx_holder[a][i] for i in list(hull.vertices)] # Remeber use the true idx
+        chq = [a,ds_hull_idx,tot_q]
+        CHQ_vec.append(chq)
+
+#	print ' the four ;;;;;;;;;;;; '
+#	print min(datasetidx_holder[a])
+#	print max(datasetidx_holder[a])
+#	print min(ds_hull_idx)
+#	print max(ds_hull_idx)
+	
+    clust_merge_plex = [] # Pairs of local clusters that need to be merged There are from the id from the datasetidx_holder labels
+    for a in range(len(CHQ_vec)):
+        # Get the first 
+        first_CHQ = CHQ_vec[a]
+        for b in xrange(a+1,len(CHQ_vec)):
+            second_CHQ = CHQ_vec[b]
+            # Find the closest points between the two hulls
+            cur_smallest_dist = 1000000. # This stays hardcoded as a maximum
+            cur_pair = []
+	    #Find the two vertex points that are closest to each other from the different clusters
+            for i in range(len(first_CHQ[1])):
+                for j in range(len(second_CHQ[1])):
+		    ## RG This is slow... USE Dist SQ to speed up...  do it yourself
+                    test_dist = distance.euclidean([dataset[first_CHQ[1][i]][0],dataset[first_CHQ[1][i]][1],dataset[first_CHQ[1][i]][2]],[dataset[second_CHQ[1][j]][0],dataset[second_CHQ[1][j]][1],dataset[second_CHQ[1][j]][2]])
+                    if  test_dist<min_dist and test_dist<cur_smallest_dist:
+                        #print 'test distance in min!!!!!!!!!!!', str(test_dist)
+                        cur_smallest_dist = test_dist
+                        cur_pair = [i,j] # This is the idx value that should corespond to dataset for this pair of hull vertices
+            if len(cur_pair)==0:
+                #we didn't get anything to match
+                continue
+	    print cur_pair   
+            # If we have a pair... look at the NN points in each hull seperatly
+            #.... then get local PCA... and compare
+            # First vertex  position in the CHQ_vec
+            clst_label_a = first_CHQ[0] # This is the label coresponding to which posiition in the holder    
+            clst_indexs_a = first_CHQ[1] # This is a list of index for this cluster... index of datasets
+            vp_idx_a = first_CHQ[1][cur_pair[0]] # This is the ds index for the vertex in question
+	    # second vertex  position in the CHQ_vec
+            clst_label_b = second_CHQ[0]        # This is the label coresponding to which posiition in the holder    
+            clst_indexs_b =  second_CHQ[1] # This is a list of index for this cluster... index of datasets
+            vp_idx_b = second_CHQ[1][cur_pair[1]] # This is the ds index for the vertex in question
+
+
+	    print 'A few things  with current stuff A', str(clst_label_a)
+	    print 'length of clusters' , str(len(clst_indexs_a))
+	    print 'length of full clusters' , str(len(datasetidx_holder[clst_label_a]))
+	    print vp_idx_a
+	    print 'A few things  with current stuff B', str(clst_label_b)
+	    print 'length of clusters' , str(len(clst_indexs_b))
+	    print 'length of full clusters' , str(len(datasetidx_holder[clst_label_b]))
+	    print vp_idx_b
+	    print ' what is the distance between the two points... see if its small ' 
+            print distance.euclidean([dataset[vp_idx_a][0],dataset[vp_idx_a][1],dataset[vp_idx_a][2]],[dataset[vp_idx_b][0],dataset[vp_idx_b][1],dataset[vp_idx_b][2]])
+
+	    # Now do the comparison
+
+            #Find First PCA 
+            local_pts_idx_a = []
+            for i in datasetidx_holder[clst_label_a]:
+            #for i in clst_indexs_a:
+                k_dist = distance.euclidean([dataset[i][0],dataset[i][1],dataset[i][2]],[dataset[vp_idx_a][0],dataset[vp_idx_a][1],dataset[vp_idx_a][2]])
+                if k_dist<k_radius:
+                    local_pts_idx_a.append(i)
+            # Find the PCA
+	    print 'size of local pca A' , str(len(local_pts_idx_a))
+            local_PCA_a = [-999]
+            local_PCA_dir_a = [-999]
+            try:
+                local_PCA_a = axfi.PCAParams(dataset,local_pts_idx_a,3)
+                local_PCA_dir_a = axfi.PCAParams_dir(dataset,local_pts_idx_a,3)
+            except:
+                print 'Opps we dont have a pca'
+                local_PCA_a = [-999]
+                continue
+
+            #Find second PCA 
+            local_pts_idx_b = []
+            #for i in clst_indexs_b:
+            for i in datasetidx_holder[clst_label_b]:
+                k_dist = distance.euclidean([dataset[i][0],dataset[i][1],dataset[i][2]],[dataset[vp_idx_b][0],dataset[vp_idx_b][1],dataset[vp_idx_b][2]])
+                if k_dist<k_radius:
+                    local_pts_idx_b.append(i)
+            # Find the PCA
+	    print 'size of local pca B' , str(len(local_pts_idx_b))
+            #local_PCA_a = [-999]  ##### This is a typo
+            local_PCA_b = [-999]
+            local_PCA_dir_b = [-999]
+            try:
+                local_PCA_b = axfi.PCAParams(dataset,local_pts_idx_b,3)
+                local_PCA_dir_b = axfi.PCAParams_dir(dataset,local_pts_idx_b,3)
+            except:
+                print 'Opps we dont have a pca'
+                local_PCA_b = [-999]
+                continue
+
+	    # Get the points 
+            vtx_A =  np.asarray([dataset[vp_idx_a][0],dataset[vp_idx_a][1],dataset[vp_idx_a][2]])
+            vtx_B =  np.asarray([dataset[vp_idx_b][0],dataset[vp_idx_b][1],dataset[vp_idx_b][2]])
+            Length_between_vtx = distance.euclidean(vtx_A,vtx_B)
+	    print ' ========OUT PUT ======='
+	    print vtx_A
+	    print vtx_B
+	    print Length_between_vtx
+            projA_plus = vtx_A + np.asarray([1,1,1])*Length_between_vtx * np.asarray(local_PCA_dir_a[0])
+            projA_minus = vtx_A - np.asarray([1,1,1])*Length_between_vtx * np.asarray(local_PCA_dir_a[0])
+            projB_plus = vtx_B + np.asarray([1,1,1])*Length_between_vtx * np.asarray(local_PCA_dir_b[0])
+            projB_minus = vtx_B - np.asarray([1,1,1])*Length_between_vtx * np.asarray(local_PCA_dir_b[0])
+            #projA_plus = vtx_A + np.asarray([1,1,1])*Length_between_vtx * np.asarray(local_PCA_a)
+            #projA_minus = vtx_A - np.asarray([1,1,1])*Length_between_vtx * np.asarray(local_PCA_a)
+            #projB_plus = vtx_B + np.asarray([1,1,1])*Length_between_vtx * np.asarray(local_PCA_b)
+            #projB_minus = vtx_B - np.asarray([1,1,1])*Length_between_vtx * np.asarray(local_PCA_b)
+	    print ' here are the prohA' 
+	    print projA_plus
+	    print ' here are the prohA' 
+	    print projA_minus
+	    print ' here are the prohB' 
+	    print projB_plus
+	    print ' here are the prohB' 
+	    print projB_minus
+
+            # Deltas
+            deltaAB_plus = distance.euclidean(projA_plus,vtx_B)
+            deltaAB_minus = distance.euclidean(projA_minus,vtx_B)
+            deltaBA_plus = distance.euclidean(projB_plus,vtx_A)
+            deltaBA_minus = distance.euclidean(projB_minus,vtx_A)
+	    print ' here are the delats AB' 
+	    print deltaAB_plus
+	    print ' here are the delats AB - ' 
+	    print deltaAB_minus
+	    print ' here are the delats BA' 
+	    print deltaBA_plus
+	    print ' here are the delats BA - ' 
+	    print deltaBA_minus
+
+	    print ' ======== E N D ======='
+
+            AB = False
+            BA = False
+            if deltaAB_plus < max_delta or deltaAB_minus <max_delta:
+                AB = True
+
+            if deltaBA_plus < max_delta or deltaBA_minus <max_delta:
+                BA = True
+
+            if not AB :
+                print 'did not pass prjection AB'
+                continue
+
+            if not BA :
+                print 'did not pass prjection BA'
+                continue
+
+            # Now we have found that these two need to get merged together. 
+            clust_merge_plex.append([clst_label_a,clst_label_b])
+	    print ' we made it!!!!!' 
+
+    print ' presort '
+    print clust_merge_plex
+    # Put together the clusters that should be merged
+    clust_merge_plex = sorted([sorted(x) for x in clust_merge_plex])
+    resultlist = []
+    if len(clust_merge_plex) >= 1: # If your list is empty then you dont need to do anything.
+        resultlist = [clust_merge_plex[0]] #Add the first item to your resultset
+        if len(clust_merge_plex) > 1: #If there is only one list in your list then you dont need to do anything.
+            for l in clust_merge_plex[1:]: #Loop through lists starting at list 1
+                listset = set(l) #Turn you list into a set
+                merged = False #Trigger
+                for index in range(len(resultlist)): #Use indexes of the list for speed.
+                    rset = set(resultlist[index]) #Get list from you resultset as a set
+                    if len(listset & rset) != 0: #If listset and rset have a common value then the len will be greater than 1
+                        resultlist[index] = list(listset | rset) #Update the resultlist with the updated union of listset and rset
+                        merged = True #Turn trigger to True
+                        break #Because you found a match there is no need to continue the for loop.
+                if not merged: #If there was no match then add the list to the resultset, so it doesnt get left out.
+                    resultlist.append(l)
+    print ' resultlist '
+    print resultlist
+
+    ####### Reassign all labels for the results list
+    print 'working in the reassign part of the code in the result list loop'
+    for pa in resultlist:
+	print ' this is pa ' 
+	print pa
+        # First one keeps the label
+        clusterlabel = pa[0]
+	print ' this is cluster label ' 
+	print clusterlabel
+        # These already should all have the same labels
+        cluster_idx_positions =  datasetidx_holder[clusterlabel]
+	print ' this is cluster idx position' 
+	print cluster_idx_positions
+        # This gets the label value for the cluster
+        labels_label = labels[cluster_idx_positions[0]]
+	print 'this is labels_label.... it should be a unique label for all of the preclustered points'
+	print labels_label
+	
+	# Test check 
+	print ' length of the labels list is this ' 
+	print len(labels)
+
+	print' we need to midify the labels list ' 
+	# loop over all of the PA;
+	for p in pa:
+	    # p is the clusterlabel
+	    cidx = datasetidx_holder[p]
+	    print ' this is the first labels and convert '
+	    for i in cidx:
+		print labels[i]
+	        labels[i] = labels_label
+		print labels[i]
+
+	#test_labels = [labels[cluster_idx_positions[x]] for x in range(len(cluster_idx_positions))]
+        print ' ' 
+    # Return here? 
+    # Currently this is only working of the labels end of things... .the new dataset is not addresed yet
+    # I just  need the labels at the moment .... and this is a fucking mess.... 
+    return datasetidx_holder,labels
+
+
+
+'''
+	# pa is an index in a holder
+
+        nclusterlabels = [ pa[x] for x in xrange(1,len(pa))]
+	idxlabels_to_change = []
+	for i in nclusterlabels:
+	    for l in datasetidx_holder[i]:
+	        idxlabels_to_change.append(l)
+		
+		
+        for lab in range(len(labels)):
+            clab = labels[lab]
+            # Loop over the ncluster labels to check
+            for tc in idxlabels_to_change:
+            #for tc in nclusterlabels:
+                if labels[tc]==clab:
+                #if tc==clab:
+                    # Make this position label value labels_label
+                    labels[lab]=labels_label
+	
+
+    new_holder = []
+    for pa in resultlist:
+	temp_new_holder = []
+	for cpos in pa:
+	    idx_positions = datasetidx_holder[cpos]
+	    [temp_new_holder.append(i) for i in idx_positions]
+        new_holder.append(temp_new_holder) 		
+
+    # Remove the older clusters from the index holde
+    remove_holder = [item for sublist in resultlist for item in sublist]
+    new_datasetidx_holder = [ datasetidx_holder[a] for a in xrange(len(datasetidx_holder)) if a not in remove_holder]
+    # Next add on the new holder
+    for i in new_holder:
+	new_datasetidx_holder.append(i)
+
+    print ' presort '
+    print clust_merge_plex
+    return new_datasetidx_holder,labels
+'''
+
+
+		
+	
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def Track_Stitcher(dataset,datasetidx_holder,labels):
     # What is tunable
     # min_dist ==> min dist for the vertex points of two different hulls to be considered
-    min_dist = 10  # RG Magic
+    min_dist = 50  # RG Magic
     # k_radius ==> radius of the sphere around the vertex point looking at clustered charge for that hull
     k_radius = 5  # RG Hard Coded
     #k_radius = 10  # RG Hard Coded
@@ -274,6 +638,7 @@ def Track_Stitcher(dataset,datasetidx_holder,labels):
         hull = ConvexHull(points_v)
         # Fill up the CHQ_V     
         ds_hull_idx = [datasetidx_holder[a][i] for i in list(hull.vertices)]
+	
         chq = [a,ds_hull_idx,tot_q]
         CHQ_vec.append(chq)
 
@@ -488,7 +853,8 @@ def Track_Stitcher_v2(dataset,datasetidx_holder,labels):
 
         if hull_check(points_v)!=3:
             # Bail out and give up on hull
-	    return datasetidx_holder, labels
+	    continue
+	    #return datasetidx_holder, labels
 
         hull = ConvexHull(points_v)
         # Fill up the CHQ_V     
@@ -733,6 +1099,7 @@ def Track_VOX_Stitcher_v2(dataset,datasetidx_holder,labels):
     max_delta = 1
     CHQ_vec = []
     # ^^^^^^ Will be of the form [    [datasetidx_holder INDEX, the vertices of the hull, total_charge] ]
+    print 'test === size of holder',  str(len(datasetidx_holder))
     for a in range(len(datasetidx_holder)):
         points_v = []
         tot_q = 0.0
@@ -752,6 +1119,7 @@ def Track_VOX_Stitcher_v2(dataset,datasetidx_holder,labels):
         CHQ_vec.append(chq)
 
     clust_merge_plex = [] # Pairs of local clusters that need to be merged There are from the id from the datasetidx_holder labels
+    print 'test === chq vec',  str(len(CHQ_vec))
     for a in range(len(CHQ_vec)):
         # Get the first 
         first_CHQ = CHQ_vec[a]
